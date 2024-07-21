ab8e77be2ae05dbc3a2d64b8f225b81cc737bb01
salie98 <salie_98@yahoo.com>
2024-07-21 23:24:50 +0800

    updated 4.6 assignment config.yml

--- controller.js
+++ controller.js
@@ -3,5 +3,16 @@
 function print(req, res){
     res.send("Hello world!");
 }
+function home(req, res){
+    res.send("this is the home page");
+}
 
-module.exports = print;
+function user(req, res) {
+    var name1 = "Danielle";
+    var email1 = "danielle@test.com";
+    var password1 = "123test";
+    var result = name1 + " " + email1 + " " + password1;
+    res.send("person info" + result);
+};
+
+module.exports = print, home, user;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-
+app.use(express.json());
 const print = require("./controller");
 
 app.get('/', print);
-
+app.get('/', home);
+app.post('/',user);
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening to port ${process.env.PORT}`)
 })
