9b6149ae1b57e9f3d931c2116e888f9e3319a1cf
salie98 <salie_98@yahoo.com>
2024-07-23 21:37:47 +0800

    updated r3

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-require("dotenv").config();
 const express = require('express')
-const app = express()
-app.use(express.json());
-const print = require("./controller");
+const router = express.Router()
 
-app.get('/', print);
-app.get('/', home);
-app.post('/',user);
-app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
+router.get('/greeting', (req, res) => {
+    const { name }  = req.query;
+    res.send('<h1> Hello :'+ name +"</h1>")
 })
+
+router.get('/greet-template', (req,res) => {
+    name = req.query.name
+    res.render('index', { user_name: name});
+})
+
+module.exports = router
+
+
+// require("dotenv").config();
+// const express = require('express')
+// const app = express()
+// app.use(express.json());
+// const print = require("./controller");
+
+// app.get('/', print);
+// app.get('/', home);
+// app.post('/',user);
+// app.listen(process.env.PORT, ()=>{
+//     console.log(`Listening to port ${process.env.PORT}`)
+// })
